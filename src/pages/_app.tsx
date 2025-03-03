d2481250e1829ad343f3fae1e98cb7268f52ed47
Fukuro <anya.androschuk@gmail.com>
2025-03-03 13:48:19 +0200

    added a favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,15 @@ import '../styles/Board.scss';
 import RootLayout from './layout';
 
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>🖌️ Doodly Do! - Online Realtime drawing app</title>
+        <link rel="icon" href="/fukuro_logo_main.svg" />
+      </Head>
     <RootLayout>
        <Component {...pageProps} />
       </RootLayout>
